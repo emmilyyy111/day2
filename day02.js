7fe22311b5e38546b7524a493aa82a8f26cd95c3
emmilyyy111 <86429560+emmilyyy111@users.noreply.github.com>
2021-07-09 21:01:39 -0400

    Add files via upload
    Calling Functions

---
+++ day02.js
@@ -0,0 +1,26 @@
+// // create a variable to hold out subtotal
+//  const subTotal = 145.64;
+//  const taxRate = 0.07
+// // create a function that calculates taxtotal from subtotal
+// function calcTaxTotal(taxRate, subtotal){
+//     const  taxTotal = taxRate * subtotal
+//     return taxTotal
+// }
+// // call the salestax function with our subtotal and save taxtotal
+// const myTaxTotal = calcTaxTotal(taxRate, subTotal) // just runs the function
+// // calculate the total = subtotal + taxtotal
+// const myTotal = subTotal + myTaxTotal
+// // display it all...
+// console.log('SubTotal ', subTotal)
+// console.log('Tax       ', myTaxTotal.toFixed(2))
+// console.log('-------------------')
+// console.log('total     ', myTotal)
+
+
+const nums = [10, 20, 30, 40, 50]
+
+for (let i = 0); i < nums.length; i++){
+console.log(nums[i])
+}
+
+
